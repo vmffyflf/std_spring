4c286484572e1c4084d6b6624e117e9846fed7c9
j__h.106 <84125506+vmffyflf@users.noreply.github.com>
2022-09-29 15:08:52 +0900

    0929-1

--- src/main/java/com/example/stdspring/controller/StdController.java
+++ src/main/java/com/example/stdspring/controller/StdController.java
@@ -1,9 +1,10 @@
 package com.example.stdspring.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.stdspring.dto.MemberDto;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/world/hello")
@@ -13,5 +14,50 @@ public class StdController {
     public String hello(@PathVariable String caps ) {
         return caps;
     }
+
+    @GetMapping(value = "/reqst")
+    public String resq(@RequestParam String name, @RequestParam String sex, @RequestParam String age){
+        return name + " " + sex + " " + age;
+    }
+
+    @GetMapping(value = "/reqst1")
+    public String resq1(@RequestParam Map<String, String> param) {
+        StringBuilder sb = new StringBuilder();
+        param.entrySet().forEach(map -> {
+            sb.append(map.getKey() + " : " + map.getValue() + "\n");
+        });
+
+        return sb.toString();
+    }
+
+    @GetMapping(value = "/reqst2")
+    public String getReqst(MemberDto memberDto) {
+        return memberDto.toString();
+    }
+
+    @PostMapping(value = "/domain")
+    public String postExam() {
+        return "Hello world!!";
+    }
+
+    @PutMapping(value = "/member")
+    public String postMember(@RequestBody Map<String, Object> putData) {
+        StringBuilder sb = new StringBuilder();
+
+        putData.entrySet().forEach(map -> {
+            sb.append(map.getKey() + " : " + map.getValue() + '\n');
+        });
+
+        return sb.toString();
+    }
+    @PutMapping(value = "/member1")
+    public String postMember1(@RequestBody MemberDto memberDto) {
+        return memberDto.toString();
+    }
+    @PutMapping(value = "/member2")
+    public String postMember2(@RequestBody MemberDto memberDto) {
+        return memberDto.toString();
+    }
+
 
 }
